895e06b191810c9077b5eb77cacf52eeccb11879
HAYDAWN <haydawn.@HAYDAWNs-iMac.local>
2024-01-17 12:49:47 +0000

    Added event listener to score.js

---
+++ assets/js/scores.js
@@ -0,0 +1,6 @@
+// Event listener for the submit button to save the score
+document.getElementById("submit").addEventListener("click", function () {
+    var initials = document.getElementById("initials").value;
+    var score = time; 
+
+  
